5c2b81c331a16bb799840d379369e8f532b8263c
Patrick Cookson <cookson.pat@gmail.com>
2013-08-31 12:39:48 -0400

    added new HMMApp

---
+++ HMMApp/src/com/example/hmmapp/backend/Song.java
@@ -0,0 +1,153 @@
+package com.example.hmmapp.backend;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Vector;
+
+public class Song {
+
+	private String artist;
+	private String title;
+	private int artistId;
+	private String albumName;
+	private int albumId;
+	private int songId;
+	private String songPath;
+	private int bpm;
+
+
+	public Song(String artist, int artistId, String albumName, int albumId, 
+			String title, int songId, String songPath){
+		this.artist = artist;
+		this.artistId = artistId;
+		this.albumName = albumName;
+		this.albumId = albumId;
+		this.title = title;
+		this.songId = songId;
+		this.songPath = songPath;
+		this.bpm = httpGetBpm();
+	}
+
+	public int getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+
+	public String getAlbumName() {
+		return albumName;
+	}
+
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+
+	public int getAlbumId() {
+		return albumId;
+	}
+
+	public void setAlbumId(int albumId) {
+		this.albumId = albumId;
+	}
+
+	public int getSongId() {
+		return songId;
+	}
+
+	public void setSongId(int songId) {
+		this.songId = songId;
+	}
+
+	public String getSongPath() {
+		return songPath;
+	}
+
+	public void setSongPath(String songPath) {
+		this.songPath = songPath;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getBpm() {
+		return bpm;
+	}
+
+	public void setBpm(int bpm) {
+		this.bpm = bpm;
+	}
+
+	private int httpGetBpm(){
+		HttpURLConnection connection = null;
+		String artistName = artist.replaceAll(" ", "-");
+		String songName = title.replaceAll(" ", "-");
+		int bpm = 0;
+		try {
+
+			URL oracle = new URL("http://www.songbpm.com/"+artistName+"/"+songName);
+			URLConnection yc = oracle.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					yc.getInputStream()));
+			String inputLine;
+			Vector<String> htmlVector = new Vector<String>();
+			while ((inputLine = in.readLine()) != null) {
+				htmlVector.add(inputLine);
+				//System.out.println(inputLine);
+
+			}
+			in.close();
+			String actualBpm = null;
+			int i=0;
+			while(htmlVector.get(i)!=null){
+				if(htmlVector.get(i).compareTo("<div class='text'>BPM</div>")==0){
+					actualBpm=htmlVector.get(i-1);
+					break;
+				}
+				i++;
+			}
+			bpm = Integer.parseInt(actualBpm.split("<div class='number'>")[1].split("</div>")[0]);
+
+
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} finally {
+			if(null != connection) { connection.disconnect(); }
+		}
+		return bpm;
+	}
+
+
+	public String toString(){
+		StringBuilder songInformation = new StringBuilder();
+		String space = " ";
+		songInformation.append(getTitle());
+		songInformation.append(space);
+		songInformation.append(getArtist());
+		songInformation.append(space);
+		songInformation.append(getBpm());
+		return songInformation.toString();
+	}
+}
---
+++ HMMApp/src/com/example/hmmapp/backend/Workout.java
@@ -0,0 +1,156 @@
+package com.example.hmmapp.backend;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Stack;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public class Workout extends Activity{
+
+	private int runningBpm;
+	private long time;
+	private Song currentSong;
+	private Map<String, Song> songMap;
+	private Stack<Song> prevStack;
+	private Stack<Song> nextStack;
+
+	public Workout(){
+		this.songMap = new HashMap<String, Song>();
+		prevStack = new Stack<Song>();
+		nextStack = new Stack<Song>();
+		start();
+	}
+
+	public void start(){
+		String[] STAR = { "*" };
+		Uri allSongUri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+		String selection = MediaStore.Audio.Media.IS_MUSIC + " !=0";
+		@SuppressWarnings("deprecation")
+		Cursor cursor = managedQuery(allSongUri, STAR, selection, null, null);
+		if(cursor.moveToFirst()){
+			do {
+				String songName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+				int songId = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media._ID));
+				String fullPath = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+
+				String albumName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+				int albumId = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
+
+				String artistName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+				int artistId = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST_ID));
+
+
+				Song newSong = new Song(artistName, artistId, albumName, albumId, songName, songId, fullPath);
+				songMap.put(artistName, newSong);
+			}while(cursor.moveToNext());
+		}
+		cursor.close();
+
+
+	}
+
+	public void end(){
+
+	}
+
+	public void setBpm(int runningBpm){
+		this.runningBpm = runningBpm;
+
+	}
+
+	public int getRunningBpm(){
+		return runningBpm;	
+	}
+
+	public void setTime(long time){
+		this.time=time;
+
+	}
+
+	public long getTime(){
+		return time;
+	}
+
+	public void setCurrentSong(Song song){
+		this.currentSong = song;
+	}
+
+	public Song getCurrentSong(){
+		return currentSong;
+	}
+
+	public void getMoreSongs(){
+		
+	}
+	
+	public void play(){
+		MediaPlayer mp = new MediaPlayer();
+		try {
+			mp.setDataSource(getCurrentSong().getSongPath());
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void pause(){
+		//function to pause song
+	}
+
+	public void resume(){
+		//funtion to resume song
+	}
+
+	public void getNextSong(){
+		//gets next song in songs stack
+		if(getCurrentSong()!=null){
+			prevStack.push(getCurrentSong());
+		}
+		setCurrentSong(nextStack.pop()); 
+	}
+
+	public void prevSong(){
+		//gets prev song in stack
+		nextStack.push(getCurrentSong());
+		setCurrentSong(prevStack.pop()) ;
+
+	}
+
+	private void makeSongList(int runningBpm){
+		int lowerThreshold = (int) (0.80 * runningBpm);
+		int upperThreshold = (int) (1.2 * runningBpm);
+		Iterator<Entry<String, Song>> it = songMap.entrySet().iterator();
+
+
+		while(it.hasNext()){
+			Song selectedSong = it.next().getValue();
+			int selectedBpm = selectedSong.getBpm();
+			if(selectedBpm > lowerThreshold && 
+					selectedBpm < upperThreshold){
+				nextStack.push(selectedSong);
+			}
+
+
+		}
+
+	}
+
+
+}
